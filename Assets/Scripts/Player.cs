2cdc731d5464ab2aca05bf837c183c786c38b236
Bjohnston07 <bwjohnst@uci.edu>
2026-01-12 19:56:54 -0800

    Finished Game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,51 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.GetKey("up"))
+        {
+            pos.y += _speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+            pos.y -= _speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            pos.x += _speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            pos.x -= _speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+
+        if (Input.GetKeyDown("space"))
+        {
+            PlantSeed(pos);
+        }
     }
 
-    public void PlantSeed ()
+    public void PlantSeed (Vector3 pos)
     {
+        if( _numSeedsLeft != 0 )
+        {
+            _numSeedsLeft -= 1;
+            _numSeedsPlanted += 1;
+
+            Instantiate(_plantPrefab,pos, _playerTransform.rotation);
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+
+        }
         
     }
 }
